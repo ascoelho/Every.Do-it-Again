06d20038ca65c56f01bd86f269c6aa70533868c4
ascoelho <anthonystefancoelho@gmail.com>
2016-05-25 11:13:53 -0400

    first commit

---
+++ Every.Do it Again/MasterViewController.h
@@ -0,0 +1,23 @@
+//
+//  MasterViewController.h
+//  Every.Do it Again
+//
+//  Created by Anthony Coelho on 2016-05-25.
+//  Copyright © 2016 Anthony Coelho. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@class DetailViewController;
+
+@interface MasterViewController : UITableViewController <NSFetchedResultsControllerDelegate>
+
+@property (strong, nonatomic) DetailViewController *detailViewController;
+
+@property (strong, nonatomic) NSFetchedResultsController *fetchedResultsController;
+@property (strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+
+
+@end
+
